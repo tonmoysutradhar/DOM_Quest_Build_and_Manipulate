e275552a35731844986fc58270f52f46594a74fb
tonmoysutradhar <tonmoykumar933@gmail.com>
2024-09-23 22:46:05 +0600

    add Donation and History Section

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
-console.log("hello");
+document.getElementById('btn-noakhali').addEventListener('click', function(event){
+    event.preventDefault();
+    console.log("hello");
+    
+
+    const donationNoakhali = +document.getElementById('donation-amount-noakhali').value;
+
+    console.log(donationNoakhali);
+
+    const myBalance = +document.getElementById('my-balance').innerText ;
+    const myTotalBalance = myBalance - donationNoakhali ;
+    document.getElementById('my-balance').innerText = myTotalBalance ;
+    console.log(myTotalBalance);
+    
+
+    const donationAmount =  +document.getElementById('total-donation').innerText ;
+    const totalDonation = donationAmount + donationNoakhali ;
+
+    document.getElementById('total-donation').innerText = totalDonation ;
+    console.log(totalDonation);
+})
